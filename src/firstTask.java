f058ac8ca106913a1a8b4a57c57b3030db4a58ee
pp957 <pp9575@yandex.ru>
2022-06-07 14:19:11 +0300

    DZ1

---
+++ src/firstTask.java
@@ -0,0 +1,11 @@
+import java.time.chrono.MinguoEra;
+
+public class firstTask {
+    public static String firstString = "234";
+    public static String secondString = "some_text";
+
+    public static void main(String[] args) {
+        int result = Integer.parseInt(firstString) + secondString.length();
+        System.out.println(result);
+    }
+}
